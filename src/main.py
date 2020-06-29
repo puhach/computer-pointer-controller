d616f73c860e9a21b048980fd4aeb01ee7bba1b5
puhach <yaroslav.puhach@gmail.com>
2020-06-29 20:16:02 +0300

    Remove yaw and pitch compensation code

--- src/main.py
+++ src/main.py
@@ -32,40 +32,12 @@ headPoseEstimator = HeadPoseEstimator(precision=args.precision, device=args.devi
 gazeEstimator = GazeEstimator(precision=args.precision, device=args.device, extensions=args.ext)
 mouseController = MouseController(precision='medium', speed='medium', failsafe=args.failsafe)
 
-for frame in feed.next_frame():
-    #face = faceDetector.extract(frame, confidence=0.5)
+for frame in feed.next_frame():    
     face, face_box = faceDetector.extract(frame, confidence=0.5)
     left_eye, right_eye = eyeDetector.extract(face)
     head_pose_angles = headPoseEstimator.estimate(face)
     gx,gy,_ = gazeEstimator.estimate(left_eye, right_eye, head_pose_angles)
-    #print(gaze_vector)
-    #gx,gy,gz = gazeEstimator.normalize(gaze_vector)
-    #gx,gy,gz = gaze_vector
 
-    #if feed.input_type == 'cam':
-    #    if head_pose_angles is not None:
-    #        
-    #        g_mag = math.sqrt(gx*gx+gy*gy+gz*gz) + 1e-4
-    #
-    #        # Yaw compensation
-    #        # (we usually tilt the head)
-    #        yaw_rad = head_pose_angles[0]*math.pi/180
-    #        gx2 = gx*math.cos(yaw_rad) + g_mag*math.sin(math.acos(gx/g_mag))*math.sin(yaw_rad)
-    #        #print(head_pose_angles[0], gx, gx2)
-    #        gx = gx2
-    #
-    #        # pitch compensation                                    
-    #        pitch_rad = head_pose_angles[1]*math.pi/180
-    #        #gy2 = math.cos(pitch_rad)*gy - g_mag*math.sin(pitch_rad)*math.sin(math.acos(gy/g_mag))
-    #        gy2 = gy*math.cos(pitch_rad) + g_mag*math.sin(math.acos(gy/g_mag))*math.sin(pitch_rad)
-    #        print(head_pose_angles[1], gy, gy2)
-    #        gy = gy2
-
-
-
-    #face = draw_vector((gx,gy,gz), face)
-    #print('norm:', gazeEstimator.normalize(gaze_vector))    
-    #mouseController.move(gaze_vector[0], gaze_vector[1])
     mouseController.move(gx,gy)
 
     if args.clean:
