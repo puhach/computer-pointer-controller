bf7e473728a8437864a9ca35582a9f70528f5c30
puhach <yaroslav.puhach@gmail.com>
2020-07-06 09:57:54 +0300

    Document the face detector methods

--- src/face_detection.py
+++ src/face_detection.py
@@ -21,11 +21,25 @@ class FaceDetector(GenericModel):
 
 
     def feed_input(self, image):
+        """
+        Preprocesses the input image and feeds it to the model for inference.
+        Depending on the inference mode, the call may be blocking or not.
+        """
         image_preprocessed = self._preprocess_input(image, width=self.input_shape[3], height=self.input_shape[2])
         super().feed_input(image_preprocessed)
 
 
-    def consume_output(self, wait, confidence=0.5):
+    def consume_output(self, confidence, wait):
+        """
+        Retrieves the face bounding box from the detection results. Returns a tuple. 
+        The first value indicates whether the result was retrieved. The second item
+        is a bounding box of the face scaled to the 0..1 range. If there is no detection 
+        with a probability higher or equal to the confidence value, the bounding box 
+        will be None. 
+
+        The wait parameter specifies whether the function has to wait for the current
+        inference request to finish in case no result is available at the moment of the call.
+        """
         consumed, detections = super().consume_output(wait)
         if consumed and detections is not None:
             face_box = self._get_bounding_box(detections, confidence=confidence)            
@@ -35,6 +49,11 @@ class FaceDetector(GenericModel):
 
 
     def preprocess_output(self, face_box, frame):
+        """
+        Scales the face bounding box from the range 0..1 to the size of the frame
+        and extracts the face image. Returns a tuple containing the face image and
+        the scaled bounding box.
+        """
         if face_box is None or frame is None or frame.size < 1:
             return None, None
         else:
